9a2ef0cb90155cf39dcfa9e282738a5bb2224c2c
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-25 23:31:54 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,50 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1...11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(subTotal)
+  puts "Your cards add up to #{subTotal}"
+  return subTotal
 end
 
 def prompt_user
-  # code #prompt_user here
+puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
+  return input
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  subTotal = deal_card + deal_card
+  display_card_total(subTotal)
 end
 
-def hit?
-  # code hit? here
+def hit?(subTotal)
+  prompt_user
+  input = get_user_input
+    if input == 's'
+      subTotal
+    elsif input == 'h'
+      subTotal += deal_card
+    else 
+      invalid_command
+      prompt_user
+      get_user_input
+    end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +52,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  subTotal = initial_round
+    until subTotal > 21
+     subTotal = hit?(subTotal)
+     display_card_total(subTotal)
+    end
+end_game(subTotal)
 end
     
